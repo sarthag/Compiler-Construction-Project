bc25fe1abdd878a81713fc67220e6992af3cf771
Niveditha Kovilakath <72668473+NivedithaKovilakath@users.noreply.github.com>
2023-02-25 16:47:45 +0530

    changed the structures

--- lexerDef.h
+++ lexerDef.h
@@ -1,42 +1,42 @@
-//All data definitions used in lexer.c
-
-#ifndef LEXERDEF_H
-#define LEXERDEF_H
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "keywordTable.h"
+#include <stdbool.h>
+//array of LHS grammar G 
 
-#define BUFFERSIZE 65535
+#define NUM_OF_RULES 7
+#define NUM_OF_TERMINALS 3
+#define NUM_OF_NONTERMINALS 5
 
-typedef struct twinBuffer{
-    char buffer1[BUFFERSIZE+1];
-    char buffer2[BUFFERSIZE+1];
-}twinBuffer;
-
+/*
+typedef enum {
+    start, module, otherModules, moduleDef, epsilon
+} non_term;
 
 typedef enum {
-    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
-} token_key;
+    ID,NUM,COLON
+} term;
+
+typedef union {
+    term t;
+    non_term nt;
+}symb;
+*/
+typedef struct rhs_struct{
+    int rhs_id;
+    bool isTerminal;
+    struct rhs_struct *nextRHS;
+} rhs;
+
+typedef struct {
+    int lhs_id;
+    rhs* firstRHS;
+} lhs; 
 
 
-token_key token_list[] = {
-    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
-};
 
 
-typedef struct token{
-    token_key tid; 
-    union{
-        char* lexeme; 
-        int num;
-        double rnum;
-    };
-    int line_no; 
-    struct token *next; // Makes the struct a linked list
-} token;
 
-ktElement keyword_table[KTSIZE];
+lhs G[NUM_OF_RULES];
 
-#endif
+//array of follow and first
+
+//array of parsing table
