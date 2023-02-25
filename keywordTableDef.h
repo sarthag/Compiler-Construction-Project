eec6a22697e23649142c91c87ac4450bfc330c3f
shreyas1209 <shreyas.sesham12@gmail.com>
2023-02-25 16:56:05 +0530

    updated lexer

--- keywordTableDef.h
+++ keywordTableDef.h
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <lexerDef.h>
 
 #define PRIME 997
 #define KTSIZE 103
 #define MOD 1e9+7
 typedef struct ktElement{
     char *lexeme;
-    int  token;
+    token_key  token;
     bool occupied;
 }ktElement;
 
