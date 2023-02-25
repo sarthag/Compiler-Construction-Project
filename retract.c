2978c915192e850585bf70d0d248c198f4a861da
Shaz Furniturewala <mohammadshaz529@gmail.com>
2023-02-26 02:50:46 +0530

    retract functions

--- retract.c
+++ retract.c
@@ -1,3 +1,25 @@
 #include "lexer.h"
 #include "lexer.h"
 #include "lexerDef.h"
+
+void retract(int n) {
+    if(forward >= buffers.buffer1 && forward <= buffers.buffer1 + BUFFERSIZE) {
+        if(forward - buffers.buffer1 < n) {
+            n -= forward - buffers.buffer1;
+            forward = buffers.buffer2 + BUFFERSIZE - n;
+        }
+        else {
+            forward -= n;
+        }
+    }
+    else if(forward >= buffers.buffer2 && forward <= buffers.buffer2 + BUFFERSIZE) {
+        if(forward - buffers.buffer2 < n) {
+            n -= forward - buffers.buffer2;
+            forward = buffers.buffer1 + BUFFERSIZE - n;
+        }
+        else {
+            forward -= n;
+        }
+    }
+    numChar -= n;
+}
