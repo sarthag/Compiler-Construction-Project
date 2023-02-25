b4e4e868be72c18002801003048d2813132eaa4c
shreyas1209 <shreyas.sesham12@gmail.com>
2023-02-25 16:58:22 +0530

    lexerDef merged

--- lexerDef.h
+++ lexerDef.h
@@ -1,36 +1,27 @@
+#ifndef LEXERDEF_H
+#define LEXERDEF_H
+
 #include <stdio.h>
-#include <stdbool.h>
-//array of LHS grammar G 
+#include <stdlib.h>
+#include <string.h>
+#include "keywordTable.h"
 
 #define BUFFERSIZE 65535
 
-/*
+typedef struct twinBuffer{
+    char buffer1[BUFFERSIZE+1];
+    char buffer2[BUFFERSIZE+1];
+}twinBuffer;
+
+
 typedef enum {
-    start, module, otherModules, moduleDef, epsilon
-} non_term;
-
-typedef enum {
-    ID,NUM,COLON
-} term;
-
-typedef union {
-    term t;
-    non_term nt;
-}symb;
-*/
-typedef struct rhs_struct{
-    int rhs_id;
-    bool isTerminal;
-    struct rhs_struct *nextRHS;
-} rhs;
-
-typedef struct {
-    int lhs_id;
-    rhs* firstRHS;
-} lhs; 
+    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
+} token_key;
 
 
-
+token_key token_list[] = {
+    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
+};
 
 
 typedef struct token{
@@ -45,3 +36,5 @@ typedef struct token{
 } token;
 
 ktElement keyword_table[KTSIZE];
+
+#endif
