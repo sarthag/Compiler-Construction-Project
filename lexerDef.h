7fea9b2866f85f23f6bac15f1f344d45debf14d8
sarthag <asarthak2002@gmail.com>
2023-02-25 11:39:42 +0530

    Enumerated tokens and created the token ll

--- lexerDef.h
+++ lexerDef.h
@@ -1,5 +1,8 @@
 //All data definitions used in lexer.c
 
+#ifndef LEXERDEF_H
+#define LEXERDEF_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,3 +14,28 @@ typedef struct twinBuffer{
     char buffer2[BUFFERSIZE+1];
 }twinBuffer;
 
+
+typedef enum {
+    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
+} token_key;
+
+
+token_key token_list[] = {
+    MINUS, NE, BO, BC, MUL, COMMENTMARK, COMMA, RANGEOP, DIV, COLON, ASSIGNOP, SEMICOL, SQBO, SQBC, PLUS, LT, DEF, DRIVERDEF, LE, EQ, GT, GE, ENDDEF, DRIVERENDDEF, NUM, RNUM, ID, AND, ARRAY, BOOLEAN, BREAK, CASE, DECLARE, DEFAULT, DRIVER, END, FOR, GET_VALUE, IN, INPUT, INTEGER, MODULE, OF, OR, PARAMETERS, PRINT, PROGRAM, REAL, RETURNS, START, SWITCH, TAKES, USE, WHILE, WITH, FALSE, TRUE
+};
+
+
+typedef struct token{
+    token_key tid; 
+    union{
+        char* lexeme; 
+        int num;
+        double rnum;
+    };
+    int line_no; 
+    struct token *next; // Makes the struct a linked list
+} token;
+
+
+
+#endif
