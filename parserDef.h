89bacc9c14f97f0ca91f30a5438a78284bd15e75
NivedithaKovilakath <niveditha.kovilakath@gmail.com>
2023-02-26 15:19:41 +0530

    parse table not working fully

--- parserDef.h
+++ parserDef.h
@@ -4,22 +4,9 @@
 
 #define NUM_OF_RULES 7
 #define NUM_OF_TERMINALS 3
-#define NUM_OF_NONTERMINALS 5
+#define NUM_OF_NONTERMINALS 4
+#define PT_COLS 4
 
-/*
-typedef enum {
-    start, module, otherModules, moduleDef, epsilon
-} non_term;
-
-typedef enum {
-    ID,NUM,COLON
-} term;
-
-typedef union {
-    term t;
-    non_term nt;
-}symb;
-*/
 typedef struct rhs_struct{
     int rhs_id;
     bool isTerminal;
@@ -36,7 +23,22 @@ typedef struct {
 
 
 lhs G[NUM_OF_RULES];
-
+int parse_table[NUM_OF_NONTERMINALS][NUM_OF_TERMINALS+1];
 //array of follow and first
 
 //array of parsing table
+
+/*
+typedef enum {
+    start, module, otherModules, moduleDef, epsilon
+} non_term;
+
+typedef enum {
+    ID,NUM,COLON
+} term;
+
+typedef union {
+    term t;
+    non_term nt;
+}symb;
+*/
