a6f53c1500090747f7015af5c357efa059ed2f6c
sarthag <asarthak2002@gmail.com>
2023-02-28 14:22:13 +0530

    fixed dependencies

---
+++ ll1_gram.h
@@ -0,0 +1,13 @@
+#ifndef LL1_GRAM_H
+#define LL1_GRAM_H
+
+
+#include "parser.h"
+
+#define MAX_LINE_LENGTH 50
+int get_id(char *str, int t);
+bool isTerm(char* str);
+void generateGrammar();
+
+
+#endif
