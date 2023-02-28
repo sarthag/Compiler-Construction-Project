21e197e331d7a210bca2029e7cd3a1ed42f79e32
half-anerd <anshi.gupta2002@gmail.com>
2023-02-28 17:05:58 +0530

    new file

---
+++ driver.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    printf("First and Follow Set Automated\n Both Lexical and Syntax analysis modules implemented\n");
+    int s;
+    scanf("%d", &s);
+    while (s != 0)
+    {
+        switch (s)
+        {
+        case 1:     //prints comment free code
+            /* code */
+            break;
+        case 2:     // prints lexer results
+            break;
+        
+        case 3:   //prints all lexical and syntatic errors, prints parse tree
+            break;
+        
+        case 4:         //prints total time taken
+
+            break;
+        default:
+            break;
+        }
+    }
+    
+}
