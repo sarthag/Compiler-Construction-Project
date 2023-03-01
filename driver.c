bcf2e8d7bba366cf737b0f100817c626bddd8b0f
half-anerd <anshi.gupta2002@gmail.com>
2023-03-01 16:51:33 +0530

    driver

--- driver.c
+++ driver.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include "lexer.h"
+
+void printtokenLL(tokenLL tkll){
+    printf("Token Count : %d\n",tkll.tokenCount);
+    token* tk = tkll.start;
+    while(tk !=NULL){
+        switch(tk -> tid){
+            case(RNUM):
+                printf("|ID: %02d | RNUM: |%f| \t LineNo: %d |\n",tk->tid,tk -> rnum , tk -> line_no);
+                break;
+            case(NUM):
+                printf("|ID: %02d | NUM: |%d| \t LineNo: %d |\n",tk->tid,tk -> num , tk -> line_no);
+                break;
+            default:
+                printf("|ID: %02d | Lexeme: |%s| \t LineNo: %d |\n",tk->tid,tk -> lexeme , tk -> line_no);
+                break;
+
+        }
+        tk = tk ->next;
+    }
+    printf("NULL\n");
+}
 
 int main(){
     printf("First and Follow Set Automated\n Both Lexical and Syntax analysis modules implemented\n");
@@ -11,9 +33,10 @@ int main(){
         case 1:     //prints comment free code
             /* code */
             break;
-        case 2:     // prints lexer results
+        case 2:
+                 // prints lexer results
+            printtokenLL()
             break;
-        
         case 3:   //prints all lexical and syntatic errors, prints parse tree
             break;
         
