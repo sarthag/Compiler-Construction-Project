bc4bb7dbeef4c35061ee77d0bd37a356c0d7f184
half-anerd <anshi.gupta2002@gmail.com>
2023-03-02 01:41:16 +0530

    first and follow sets made

--- rough.py
+++ rough.py
@@ -1,8 +1,12 @@
-non_terminals = []
+import json
+
+prod_rules = []
 with open('grammar.txt', 'r') as f:
     for line in f.readlines():
-        if line.split()[0] not in non_terminals:
-            non_terminals.append(line.split()[0])
-with open('non_terminals.txt', 'w') as f:
-    for non_terminal in non_terminals:
-        f.write(non_terminal + '\n')
+        tmp = {
+            'left': line.split()[0],
+            'right': line.split()[1:]
+            }
+        prod_rules.append(tmp)
+with open('grammar.json', 'w') as f:
+    json.dump(prod_rules, f)
