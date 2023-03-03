0d9ab77aec384ac099ea6c7970f0dd555b0991cc
Shaz Furniturewala <mohammadshaz529@gmail.com>
2023-03-03 17:03:57 +0530

    corrected lexer

--- driver.c
+++ driver.c
@@ -6,7 +6,7 @@
 void printtokenLL(tokenLL tkll){
     printf("Token Count : %d\n",tkll.tokenCount);
     token* tk = tkll.start;
-    while(tk !=NULL){
+    while(tk != tkll.end -> next){
         switch(tk -> tid){
             case(RNUM):
                 printf("|LineNo: %d | RNUM: |%f| \t ID: %02d |\n", tk -> line_no,tk -> rnum ,tk->tid);
@@ -32,7 +32,7 @@ int main(){
     printf("3. Results of compilation\n");
     printf("4. Time analysis\n");
     int s;
-    char* filename = "testcode";
+    char* filename = "test_cases (stage_1)/t6(with_syntax_errors).txt";
     FILE* prog; 
     clock_t start_time, end_time, t;
     double total_CPU_time, total_CPU_time_in_seconds;
