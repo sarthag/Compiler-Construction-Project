d4dd69082e4adaa8adc13248838c15eaadfbd685
sarthag <asarthak2002@gmail.com>
2023-04-01 19:35:46 +0530

    created function headers

--- astadt.h
+++ astadt.h
@@ -2,4 +2,16 @@
 #define ASTADT_H 
 
 #include "astadtDef.h"
+
+void initASTStack(astStack* s);
+astStackNode* createastStackNode(astNode *treeloc);
+void pushast(astStack* s, astNode *treeloc);
+void popast(astStack* s);
+
+ast *createSyntaxTree();
+astNode* createASTNode(labels label, int rule_no);
+void insertASTchild(astNode *parent, astNode* child);
+void setASTSibling(astNode *node, astNode *sibling);
+void setASTparent(astNode *node, astNode *parent) ;
+
 #endif
