10fca1fe2b2c93925e962946108bcd815b603932
half-anerd <anshi.gupta2002@gmail.com>
2023-04-11 03:36:29 +0530

    trying more

--- typeChecking.c
+++ typeChecking.c
@@ -1,23 +1,29 @@
 #include "symbolTable.h"
 
-void staticTypeChecking(astNode * current){
+void staticTypeChecking(astNode * current, symbolTable * table){
     if(current->nodeType == TERMINAL){
         switch (current->name.t.tid)
         {
         case 10:
-            if(current->leftChild->nodeType == TERMINAL){
-                if(current->leftChild->rightSibling == TERMINAL){
-                    if(gettypeFromtid(current->leftChild, globalTable).varType.primitiveType == gettypeFromtid(current->leftChild->rightSibling, globalTable).varType.primitiveType){
-                        return gettypeFromtid(current->leftChild, globalTable).varType.primitiveType;
+            if(current->leftChild->name.t.tid == ID){
+                entryDataType locationType = searchSymbolTable(current->leftChild->pt->element.t.lexeme, table)->entry_DT;
+                if(locationType.isArray != 1){
+                    if(current->leftChild->rightSibling->name.t.tid == ID){
+                        if(locationType.varType.primitiveType == gettypeFromtid(current->leftChild->rightSibling, globalTable).varType.primitiveType){
+                            return gettypeFromtid(current->leftChild, globalTable).varType.primitiveType;
+                        }
+                        else{
+                            printf("type mismatch");
+                            return;
+                        }
                     }
-                    else{
-                        printf("type mismatch");
-                        return;
+                    else if (current->leftChild->rightSibling->name.t.tid == NUM){
+
                     }
                 }
-                else{
-                    
-                }
+            }
+            else{
+                printf("assigning to  non-variable entity");
             }
             break;
         
