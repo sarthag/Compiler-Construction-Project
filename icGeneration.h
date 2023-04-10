ba3ca383409f47d108025577c0c031f9c9ce1f0d
shreyas1209 <shreyas.sesham12@gmail.com>
2023-04-11 03:41:03 +0530

    Tararaa its the mfkin DO double G

--- icGeneration.h
+++ icGeneration.h
@@ -2,7 +2,7 @@
 #define ICGENERATION_H 
 
 #include "icGenerationDef.h"
-symbolRecord* insertIntoTempTable(symbolTable * parentTable, dType tempType);
+symbolRecord* insertIntoTempTable(symbolTable * parentTable, entryDataType tempType);
 symbolRecord* findFromST(char* recordName, symbolTable* table);
 void createICG(astNode* node, symbolTable* table);
 void generateIC();
