c35c03c6388be3a93ba2fe0d110110f0f4cfc6f9
NivedithaKovilakath <niveditha.kovilakath@gmail.com>
2023-04-11 18:14:54 +0530

    function rules

--- codeGen.c
+++ codeGen.c
@@ -64,6 +64,8 @@ void generateASMCode(FILE* asmFile){
 }
 
 
+/*
+
 void readWriteST(bool isRead, int width, symbolRecord* rec, FILE* asmFile){
     char b[8];
     switch (width)
@@ -101,4 +103,6 @@ void readWriteST(bool isRead, int width, symbolRecord* rec, FILE* asmFile){
         fprintf(asmFile, "\tPOP EBP\n");
     }
 }
+
+*/
 
